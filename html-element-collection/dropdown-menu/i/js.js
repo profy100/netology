0a58c23c7d6558d9d9b67582dc025783cf72fdc4
Tick <profy100@rambler.ru>
2018-05-01 09:47:26 +0300

    'Исправил'

--- html-element-collection/dropdown-menu/i/js.js
+++ html-element-collection/dropdown-menu/i/js.js
@@ -1,14 +1,9 @@
 'use strict';
 
 const button = document.getElementsByClassName('wrapper-dropdown')[0];
-console.log(button);
+
 function list (){	
-	if (button.classList.contains('active')){
-		button.classList.remove('active');
-		return
-	}
-	button.classList.add('active');
-	
+	button.classList.toggle('active');	
 }
-console.log(button.classList);
+
 button.onclick = list;
