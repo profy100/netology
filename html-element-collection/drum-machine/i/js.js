2e8fec88f37eb018120afca51670a2a753d9d0f7
Tick <profy100@rambler.ru>
2018-05-01 10:02:38 +0300

    'Исправил'

--- html-element-collection/drum-machine/i/js.js
+++ html-element-collection/drum-machine/i/js.js
@@ -5,8 +5,9 @@ const clap = document.getElementsByClassName('drum-kit__drum');
 for(let i of clap){
 	i.onclick = playSound;
 }
-
+console.log(clap);
 function playSound(){
+	this.getElementsByTagName('audio')[0].currentTime = 0;
 	this.getElementsByTagName('audio')[0].play();
 }
 
