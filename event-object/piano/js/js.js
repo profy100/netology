0559806187d23432adda31677896bf94296baefc
Tick <profy100@rambler.ru>
2018-05-05 10:10:15 +0300

    'Исправлена_Задача_Пианино'

--- event-object/piano/js/js.js
+++ event-object/piano/js/js.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const piano = document.getElementsByClassName('set middle')[0];
+const piano = document.getElementsByClassName('set')[0];
+console.log(piano);
 const buttonPiano = piano.getElementsByTagName('li');
 const arrayButtonPiano  = Array.from(buttonPiano);
 const sound = {lower: ['sounds/lower/first.mp3' ,
@@ -24,11 +25,11 @@ let key = '';
 function playPiano(event){
 	let index = arrayButtonPiano.indexOf(this);
 	let button = this.getElementsByTagName('audio')[0];
-	if(key === 'lower'){
+	if(piano.classList.contains('lower')){
 		button.src = sound.lower[index];
 		button.currentTime = 0;
 		button.play();
-	}else if(key === 'higher'){
+	}else if(piano.classList.contains('higher')){
 		button.src = sound.higher[index];
 		button.currentTime = 0;
 		button.play();
@@ -41,14 +42,18 @@ function playPiano(event){
 
 function push(event){
 	if(event.shiftKey && event.repeat){
-		key = 'lower';
+		piano.classList.remove('middle');
+		piano.classList.add('lower');
 	}else if(event.altKey && event.repeat){
-		key = 'higher';
+		piano.classList.remove('middle');
+		piano.classList.add('higher');
 	}	
 }
 
 function pull(){
-	key = 'middle';
+	piano.classList.remove('lower');
+	piano.classList.remove('higher');
+	piano.classList.add('middle');
 }
 
 document.addEventListener('keydown', push);
